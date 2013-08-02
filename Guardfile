5511510f591544d3ee1e648826c84b58b6b0a136
adrien-f <adrien.fillon@gmail.com>
2013-08-03 00:24:58 +0200

    Add Guardfile, docker-py, font-awesome, ...
    Also comes with Flat UI colors and some initial view debug

---
+++ Guardfile
@@ -0,0 +1,11 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'less', :all_on_start => true, :all_after_change => true do
+  watch(%r{quais/static/css/.+\.less$})
+end
+
+guard 'livereload' do
+  watch(%r{quais/static/css/.+\.(css)})
+  watch(%r{quais/templates/.+\.(html)})
+end
--- quais/main.py
+++ quais/main.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import docker
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def quais():
-    return render_template('quais.html')
+    d = docker.Client(base_url='http://10.0.42.1:4243', version='1.3')
+    containers = [c for c in d.containers()]
+    return render_template('quais.html', containers=containers)
+
+
+@app.template_filter('cut')
+def cut_filter(s, at):
+    return s[:at]
