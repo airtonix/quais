bccd75b9723403ef37fc3e618c520e09ace7f29e
Adrien Fillon <adrien@kaizen-developments.com>
2013-08-05 15:54:44 +0200

    I'm bad, this should fix CI detection

--- quais/extensions.py
+++ quais/extensions.py
@@ -7,7 +7,8 @@ db = SQLAlchemy()
 try:
     if os.getenv('CI_SERVER', 'no') == 'yes':
         docker_host = 'http://127.0.0.1:4243/'
-    docker_host = os.getenv('DOCKER_HOST', None)
+    else:
+	docker_host = os.getenv('DOCKER_HOST', None)
     if docker_host is None:
         raise EnvironmentError(
             'Could not find DOCKER_HOST environment variable')
